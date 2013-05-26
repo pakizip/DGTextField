d2137a248abac1fbb838b00a2d21668f02174032
Pasquale Ambrosini <pakizip@gmail.com>
2013-05-27 00:12:19 +0200

    May 27 Update Added support to all UITextFieldViewMode for rightView.

--- DGTextField.h
+++ DGTextField.h
@@ -38,4 +38,5 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 - (void)setRightButtonImage:(UIImage*)image;
 - (void)setRightButtonImage:(UIImage*)image viewMode:(UITextFieldViewMode)mode;
 
+
 @end
